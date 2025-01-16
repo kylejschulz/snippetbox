1f46b544ae791259607d6b383f9babc419a1d5e0
Kyle Schulz <kyleschulz@Kyles-MacBook-Air.local>
2025-01-16 15:22:10 -0500

    add chapter 3, 4, and 5 to repo

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-    "fmt"
+   	"errors"
+		"fmt"
     "html/template"
     "net/http"
     "strconv"
+
+		"snippetbox.kyleschulz.net/internal/models"
 )
 
 
@@ -13,6 +16,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request ) {
     app.notFound(w)
     return
   }
+
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+
 
   files := []string{
     "./ui/html/base.tmpl",
@@ -26,33 +37,86 @@ func (app *application) home(w http.ResponseWriter, r *http.Request ) {
     return
   }
 
-  err = ts.ExecuteTemplate(w,"base", nil)
+	// Create an instance of a templateData struct holding the slice of
+	// snippets.
+	data := &templateData{
+		Snippets: snippets,
+	}
+
+  err = ts.ExecuteTemplate(w,"base", data)
   if err != nil {
     app.serverError(w, err)
   }
-
-  // w.Write([]byte("Hello from Snippetbox"))
 }
 
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request ) {
-  id, err := strconv.Atoi(r.URL.Query().Get("id"))
-  if err != nil || id < 1 {
-    app.notFound(w)
-    return
-  }
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil || id < 1 {
+				app.notFound(w)
+				return
+		}
 
-  fmt.Fprintf(w, "Display a specific sniper with ID %d...", id)
+		snippet, err := app.snippets.Get(id)
+		if err != nil {
+				if errors.Is(err, models.ErrNoRecord) {
+						app.notFound(w)
+				} else {
+						app.serverError(w, err)
+				}
+				return
+		}
+
+		files := []string{
+				"./ui/html/base.tmpl",
+				"./ui/html/partials/nav.tmpl",
+				"./ui/html/pages/view.tmpl",
+		}
+
+		// Parse the template files...
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+				app.serverError(w, err)
+		return
+		}
+
+		// Create an instance of a templateData struct holding the snippet data.
+		data := &templateData{
+				Snippet: snippet,
+		}
+
+		// And then execute them. Notice how we are passing in the snippet
+		// data (a models.Snippet struct) as the final parameter?
+		err = ts.ExecuteTemplate(w, "base", data)
+		if err != nil {
+				app.serverError(w, err)
+		}
 }
+
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request ) {
 
-  if r.Method != http.MethodPost {
-    w.Header().Set("Allow", http.MethodPost)
-    app.clientError(w, http.StatusMethodNotAllowed)
-    return
-  }
+		if r.Method != http.MethodPost {
+			w.Header().Set("Allow", http.MethodPost)
+			app.clientError(w, http.StatusMethodNotAllowed)
+			return
+		}
 
-  w.Write([]byte("Create a new snippet..."))
+		// Create some variables holding dummy data. We'll remove these later on
+		// during the build.
+		title := "O snail"
+		content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+		expires := 7
+
+		// Pass the data to the SnippetModel.Insert() method, receiving the
+		// ID of the new record back.
+		id, err := app.snippets.Insert(title, content, expires)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		// Redirect the user to the relevant page for the snippet.
+		http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
 
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,6 +6,7 @@ import (
     "log"
     "net/http"
 	  "os"
+		"snippetbox.kyleschulz.net/internal/models"
 
     _ "github.com/go-sql-driver/mysql"
 )
@@ -13,28 +14,35 @@ import (
 type application struct {
     errorLog *log.Logger
     infoLog * log.Logger
+		snippets *models.SnippetModel
 }
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
 
-    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+		dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
     flag.Parse()
 
     infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-    db, err := openDB(*dsn)
-    if err != nil {
-      errorLog.Fatal(err)
-    }
+		// To keep the main() function tidy I've put the code for creating a connection
+		// pool into the separate openDB() function below. We pass openDB() the DSN
+		// from the command-line flag.
+		db, err := openDB(*dsn)
+		if err != nil {
+				errorLog.Fatal(err)
+		}
 
-    defer db.Close()
+		// We also defer a call to db.Close(), so that the connection pool is closed
+		// before the main() function exits.
+		defer db.Close()
 
     app := &application {
         errorLog: errorLog,
         infoLog: infoLog,
+				snippets: &models.SnippetModel{DB: db},
     }
 
     srv := &http.Server{
@@ -44,11 +52,11 @@ func main() {
     }
 
     infoLog.Printf("Starting server on %s", *addr)
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     errorLog.Fatal(err)
-  }
+ }
 
-  func openDB(dsn, string) (*sql.DB, error) {
+func openDB(dsn string) (*sql.DB, error) {
     db, err := sql.Open("mysql", dsn)
     if err != nil {
       return nil, err
@@ -57,4 +65,4 @@ func main() {
       return nil, err
     }
     return db, nil
-  }
+}
