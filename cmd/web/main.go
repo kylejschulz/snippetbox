c1d05fc289d999629ca8a0494c10c65cebb37088
Kyle Schulz <kyle.schulz.mail@gmail.com>
2025-01-21 17:18:02 -0500

    add chapter 10 code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+		"crypto/tls"
     "database/sql"
     "flag"
     "log"
@@ -54,6 +55,8 @@ func main() {
 		sessionManager.Store = mysqlstore.New(db)
 		sessionManager.Lifetime = 12 * time.Hour
 
+		sessionManager.Cookie.Secure = true
+
 		app := &application{
 			errorLog: errorLog,
 			infoLog: infoLog,
@@ -63,14 +66,22 @@ func main() {
 			sessionManager: sessionManager,
 		}
 
+		tlsConfig := &tls.Config{
+			CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		}
+
 		srv := &http.Server{
         Addr: *addr,
         ErrorLog: errorLog,
         Handler: app.routes(),
+				TLSConfig: tlsConfig,
+				IdleTimeout: time.Minute,
+				ReadTimeout: 5 * time.Second,
+				WriteTimeout: 10 * time.Second,
     }
 
     infoLog.Printf("Starting server on %s", *addr)
-    err = srv.ListenAndServe()
+    err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
     errorLog.Fatal(err)
  }
 
